72d5a81867415eac6f1ac9e09a5d59709f41db7f
Ryutaro0802 <ne200152g@gmail.com>
2020-07-01 18:12:08 +0900

    マークダウンを読み込む

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { attributes, react as HomeContent } from '../content/home.md';
+
+const { title, cats } = attributes;
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <article>
+      <h1>{title}</h1>
+      <HomeContent />
+      <ul>
+        {cats.map((cat, k) => (
+          <li key={k}>
+            <h2>{cat.name}</h2>
+            <p>{cat.description}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
   </Layout>
 )
 
